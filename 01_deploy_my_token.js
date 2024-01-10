1ff7a132d66bd8f843c99d709896059fa5f3d8ff
SaeeDawod <122533407+SaeeDawod@users.noreply.github.com>
2024-01-10 17:47:26 +0200

    Update 01_deploy_my_token.js

--- 01_deploy_my_token.js
+++ 01_deploy_my_token.js
@@ -1,19 +1,47 @@
-// deploy/01_deploy_my_token.js
-const { ethers } = require("hardhat");
+//  import { run } from 'hardhat';
+import { DeployFunction } from 'hardhat-deploy/types';
 
-module.exports = async ({ getNamedAccounts, deployments }) => {
-
+const deployMyToken: DeployFunction = async ({ getNamedAccounts, deployments }) => {
   console.log("Starting deployment script...");
 
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  console.log('secerthash',secretHash);
+  if (!deployer) {
+    console.error(
+      '\n\nERROR!\n\nThe node you are deploying to does not have access to a private key to sign this transaction. Add a private key in this application and activate it on the node you deployed this Smart Contract Set IDE on. If a private key already exists in this application, ensure that it is activated on the node you deployed this Smart Contract Set IDE on.\n\n'
+    );
+    process.exit(1);
+  }
 
   await deploy('MyToken', {
     from: deployer,
-    args: [deployer], // replace with actual constructor arguments
+    args: [], // replace with actual constructor arguments
     log: true,
   });
+
+  // let hasEtherScanInstance = false;
+  // try {
+  //    run('verify:get-etherscan-endpoint');
+  //   hasEtherScanInstance = true;
+  // } catch (e) {
+  //   // ignore
+  // }
+  // if (hasEtherScanInstance) {
+  //   await run('sourcify');
+  //   if (!config.verify?.etherscan?.apiKey) {
+  //     console.error(
+  //       `\n\nERROR!\n\nYou have not set your Etherscan API key in your hardhat.config.ts file. Set it and run\n\npnpm hardhat --network '${network.name}' etherscan-verify\n\n`
+  //     );
+  //   } else {
+  //     await new Promise((resolve) => setTimeout(resolve, 10 * 1000)); // allow etherscan to catch up
+  //     await run('etherscan-verify');
+  //   }
+  // }
+
+  // console.log('Deployment complete.');
 };
-module.exports.tags = ['MyToken'];
+
+export default deployMyToken;
+
+deployMyToken.tags = ['MyToken'];
